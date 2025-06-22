fc6de1632f88811333e749a9d5f095f171615cd0
SmArTeStONE1 <Aayush.Dharmadhikari@outlook.com>
2025-06-22 12:21:52 -0400

    Add files via upload

---
+++ robointro.py
@@ -0,0 +1,10 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+
+
+
+tom = Robot("Tom")
+jerry = Robot("Jerry")
+print('Hi my name is',tom.name)  # Output: Tom
+print('Hi my name is',jerry.name)  # Output: Jerry
