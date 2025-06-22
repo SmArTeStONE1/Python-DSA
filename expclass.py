e894ac3b198d6c238abb0a8fb983ad68012043bd
SmArTeStONE1 <Aayush.Dharmadhikari@outlook.com>
2025-06-22 12:28:59 -0400

    Add files via upload

---
+++ expclass.py
@@ -0,0 +1,8 @@
+class Exp:
+    def __init__(self, num1, num2, num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+
+exp1 = Exp(10, 20, 30)
+print("The sum is:", exp1.num1 + exp1.num2 + exp1.num3)
